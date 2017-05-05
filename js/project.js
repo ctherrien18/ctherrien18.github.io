2ecc17a281919fba3b5113e9efb81c506592078c
ctherrien18 <ctherrien18@jcu.edu>
2017-05-05 15:23:24 -0400

    My First Commit

---
+++ js/project.js
@@ -0,0 +1,17 @@
+/*$(document).ready(function(){
+
+    $( '.cmp-link' ).hover(function() {
+         if($( '.cmp-details' ).is(":visible")){
+              $('.cmp-details').hide();
+         } else{
+              $('.cmp-details').show();
+         }
+
+    });
+    
+});*/
+
+
+function myFunction() {
+    alert("I am an alert box!");
+}
