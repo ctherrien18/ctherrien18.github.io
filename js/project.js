4d0f002799ba6084dcb42670c14d4d1c74c03179
ctherrien18 <ctherrien18@jcu.edu>
2017-05-11 23:52:35 -0400

    final content/style  changes

--- js/project.js
+++ js/project.js
@@ -24,3 +24,23 @@ $(document).ready(function() {
     $(this).addClass(".img-anim-selected");
   });
 });
+
+// The function actually applying the offset
+function offsetAnchor() {
+  if (location.hash.length !== 0) {
+    window.scrollTo(window.scrollX, window.scrollY - 80);
+  }
+}
+
+// Captures click events of all a elements with href starting with #
+$(document).on('click', 'a[href^="#"]', function(event) {
+  // Click events are captured before hashchanges. Timeout
+  // causes offsetAnchor to be called after the page jump.
+  window.setTimeout(function() {
+    offsetAnchor();
+  }, 0);
+});
+
+// Set the offset when entering page with hash present in the url
+window.setTimeout(offsetAnchor, 0);
+
